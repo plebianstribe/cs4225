05099d2a8989372d79b6c4ba983d6b9d0fcb6428
plebianstribe <nic.holas001@outlook.com>
2023-03-05 00:55:21 +0800

    TopK MapReduce1 Iter 1

--- 1commonWordsAssgn/TopkCommonWords.java
+++ 1commonWordsAssgn/TopkCommonWords.java
@@ -7,6 +7,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.FileSystem;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -42,6 +45,7 @@ public class TopkCommonWords {
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
+            /*
             //splits value which is input to individual tokens
             StringTokenizer itr = new StringTokenizer(value.toString());
 
@@ -50,31 +54,134 @@ public class TopkCommonWords {
                 word.set(itr.nextToken());
                 context.write(word, one);
             }
+            */
 
-            //test
+            //Makes an array of individual words split by separators give
+            //Runs through array and writes output for each entry IF it does not appear in stopwords AND longer than 4 characters
             String[] values = value.toString().split(separator);
-            System.out.println(values);
+            String[] stopArray = stopwords.split("\s+");
+            List<String> stopList = new ArrayList<>(Arrays.asList(stopArray));
+            for (String str : itr) {
+                if (str.length() > 4) {
+                    if (!stopList.contains(str)) {
+                        word.set(str);
+                        context.write(word, one);
+                    }
+                }
+            }
         }
     }
 
-    public static class IntSumReducer
+    public static class IntCountAll
             extends Reducer<Text,IntWritable,Text,IntWritable> {
         private IntWritable result = new IntWritable();
 
         public void reduce(Text key, Iterable<IntWritable> values,
                            Context context
         ) throws IOException, InterruptedException {
-            int sum = 0;
+            int sum = -1;
+
+            //input values should be Iterable Int thing, idk what is iterable but it should be "Hello":{5, 2}
+
+            //For each item in the input, check if have {a, b}.
+            //Case 1: have {a, b}. Then have to sum to {sum(a), sum(b), and write 1 value which is max(sum(a), sum(b))
+            //Case 2: have only one value. then just sum that value and return
             for (IntWritable val : values) {
-                sum += val.get();
+                int valI = val.get();
+                if(sum == -1){
+                    sum = valI;
+                }
+                else if(valI<sum){
+                    sum = valI;
+                }
+
             }
             result.set(sum);
             context.write(key, result);
         }
     }
+
+    public static class SortMap
+            extends Mapper<Object, Text, IntWritable, Text>{
+        private Text word = new Text();
+        private String separator = new String();
+        private String stopwords = new String();
+
+        public void setup(Configuration conf) {
+            /*InputStream is = FileSystem.get(conf).open(new Path(conf.get("stopwords.path")));
+
+            System.out.println(is);
+            System.out.println(is.getClass());
+            */
+
+            stopwords = conf.get("Separator.stopwords");
+            separator = conf.get("Separator.common");
+        }
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            /*
+            //splits value which is input to individual tokens
+            StringTokenizer itr = new StringTokenizer(value.toString());
+
+            //iterates through each token to add the word and its count to context (which is a dict?)
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+            */
+
+            //Makes an array of individual words split by separators give
+            //Runs through array and writes output for each entry IF it does not appear in stopwords AND longer than 4 characters
+            String[] values = value.toString().split(separator);
+            String[] stopArray = stopwords.split("\s+");
+            List<String> stopList = new ArrayList<>(Arrays.asList(stopArray));
+            for (String str : itr) {
+                if (str.length() > 4) {
+                    if (!stopList.contains(str)) {
+                        word.set(str);
+                        context.write(word, one);
+                    }
+                }
+            }
+        }
+    }
+
+    public static class SortReduce
+            extends Reducer<IntWritable,Text,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Iterable<IntWritable> key, Text values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = -1;
+
+            //input values should be Iterable Int thing, idk what is iterable but it should be "Hello":{5, 2}
+
+            //For each item in the input, check if have {a, b}.
+            //Case 1: have {a, b}. Then have to sum to {sum(a), sum(b), and write 1 value which is max(sum(a), sum(b))
+            //Case 2: have only one value. then just sum that value and return
+            for (IntWritable val : values) {
+                int valI = val.get();
+                if(sum == -1){
+                    sum = valI;
+                }
+                else if(valI<sum){
+                    sum = valI;
+                }
+
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-
+        /*
+        FileSystem fs = FileSystem.get(conf);
+        Path interDirPath = new Path("/home/course/cs4225/cs4225_assign/temp/assign1_inter/A0223939W"); // REPLACE THIS WITH YOUR OWN ID!
+        */
         Path stopPath = new Path(args[2]);
         InputStream is = new FileInputStream(args[2]);
         byte[] array = new byte[100];
@@ -82,13 +189,13 @@ public class TopkCommonWords {
         String data = new String(array);
 
         conf.set("Separator.stopwords", data);
-        conf.set("Separator.common", "//s");
+        conf.set("Separator.common", "\s+");
 
         //\s\t\n\r\f
         Job job = Job.getInstance(conf, "Top k Common Words");
         job.setJarByClass(TopkCommonWords.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setCombinerClass(IntCountAll.class);
+        job.setReducerClass(IntCountAll.class);
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
         job.setOutputKeyClass(Text.class);
@@ -96,7 +203,29 @@ public class TopkCommonWords {
         MultipleInputs.addInputPath(job,new Path(args[0]), TextInputFormat.class, TokenizerMapper.class);
         MultipleInputs.addInputPath(job,new Path(args[1]), TextInputFormat.class, TokenizerMapper.class);
         //MultipleInputs.addInputPath(job,new Path(args[1]), TextInputFormat.class, TokenizerMapper2.class);
+        //FileOutputFormat.setOutputPath(job, interDirPath);
         FileOutputFormat.setOutputPath(job, new Path(args[3]));
+
+    /*
+        job.waitForCompletion(true);
+
+        Configuration conf2 = new Configuration();
+        conf2.setInt("k", new Int(args[4]));
+        Job job2 = Job.getInstance(conf2, "Sorting");
+        job2.setJarByClass(TopkCommonWords.class);
+        job2.setMapperClass(SortMap.class)
+        job2.setReducerClass(SortReduce.class);
+        job2.setMapOutputKeyClass(IntWritable.class);
+        job2.setMapOutputValueClass(Text.class);
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job2, interDirPath);
+        FileOutputFormat.setOutputPath(job2, new Path(args[3]));
+
+        boolean hasCompleted = job2.waitForCompletion(true);
+        fs.delete(interDirPath, true); // ONLY call this after your last job has completed to delete your intermediate directory
+        System.exit(hasCompleted ? 0 : 1); // there should be NO MORE code below this line
+     */
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
