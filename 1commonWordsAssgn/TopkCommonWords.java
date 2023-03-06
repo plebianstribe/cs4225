9e3e65f586abe4d26349871ab40856af753f74e0
plebianstribe <nic.holas001@outlook.com>
2023-03-06 22:30:44 +0800

    TopK MapReduce1 Iter 66 localFS

--- 1commonWordsAssgn/TopkCommonWords.java
+++ 1commonWordsAssgn/TopkCommonWords.java
@@ -264,7 +264,7 @@ public class TopkCommonWords {
 
         FileSystem fs = FileSystem.get(conf);
         //Path interDirPath = new Path("/home/course/cs4225/cs4225_assign/temp/assign1_inter/A0223939W"); // REPLACE THIS WITH YOUR OWN ID!
-        Path interDirPath = new Path("/cs4225/assign1_inter/A0223939W");
+        Path interDirPath = new Path(args[3]+"/A0223939W");
 
         Path path = new Path(args[2]);
         BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(path)));
