83a1a1058adc3d9b5209bae98fc127f1897e62ed
plebianstribe <nic.holas001@outlook.com>
2023-03-06 23:17:53 +0800

    TopK MapReduce1 Iter 69 localFS

--- 1commonWordsAssgn/TopkCommonWords.java
+++ 1commonWordsAssgn/TopkCommonWords.java
@@ -308,6 +308,7 @@ public class TopkCommonWords {
         FileInputFormat.addInputPath(job2, interDirPath);
         FileOutputFormat.setOutputPath(job2, new Path(args[3]));
 
+        boolean hasCompleted = job2.waitForCompletion(true);
         fs.delete(interDirPath, true); // ONLY call this after your last job has completed to delete your intermediate directory
         System.exit(hasCompleted ? 0 : 1); // there should be NO MORE code below this line
         //System.exit(job.waitForCompletion(true) ? 0 : 1);
